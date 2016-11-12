83996ee4310b34a17377bed6d3fcd472d5127e93
Jesse Noseworthy <noseworthyjesse@gmail.com>
2016-11-12 17:14:46 -0500

    more content

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -12,8 +12,10 @@ var heroSlide = function heroSlide() {
 var flickityDotNames = function flickityDotNames() {
   $(".dot").each(function () {
     $('.dot:first-child').html('Resume');
-    $('.dot:nth-child(2)').html('Projects/Programs');
-    $('.dot:last-child').html('References');
+    $('.dot:nth-child(2)').html('Projects');
+    $('.dot:nth-child(3)').html('References');
+    $('.dot:nth-child(4)').html('Media');
+    $('.dot:last-child').html('Contact');
   });
 };
 
