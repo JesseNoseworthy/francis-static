f43a45dc1f4c8c1da63101baf7eb4ac3f984c31e
Jesse Noseworthy <noseworthyjesse@gmail.com>
2016-11-12 15:27:47 -0500

    slide out nav class for nav

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -9,3 +9,26 @@ console.log('working');
 $('.continue').on('click', function () {
   $(".hero").slideUp(1250);
 });
+
+$(".sidebar-link").each(function (index) {
+  $(this).on('click', function () {
+    $('.active').removeClass('active');
+    $(this).addClass('active');
+  });
+});
+
+// $('.sidebar-link-about').on('click', function () {
+//   $(this).addClass('active');
+//   if ($(this).hasClass('active')) {
+//     console.log('acti');
+//     $(".about").toggleClass('active');
+//   }
+// });
+
+// $('.sidebar-link-resume').on('click', function () {
+//   $(this).addClass('active');
+//   if ($(this).hasClass('active')) {
+//     console.log('acti');
+//     $(".resume").toggleClass('active');
+//   }
+// });
